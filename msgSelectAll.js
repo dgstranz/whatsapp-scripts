ed9b4507fa65a1070d33922aca16fd491ae7a66f
dgstranz <dgstranz@gmail.com>
2016-06-03 17:09:35 +0200

    added message deletion

--- msgSelectAll.js
+++ msgSelectAll.js
@@ -1,4 +1,30 @@
-var list = document.getElementsByClassName('unchecked');
-for (var i = list.length - 1; i >= 0; i--) {
-	list[i].parentElement.click();
-}
+(function() {
+	function scrollToTop() {
+		document.getElementsByClassName('pane-chat-body')[0].scrollTop = 0;
+	}
+
+	function displayCheckboxes() {
+		document.getElementsByClassName('icon-menu')[1].click();
+		document.getElementsByClassName('menu-shortcut')[1].click();
+	}
+
+	function checkBoxes() {
+		var list = document.getElementsByClassName('unchecked');
+		var length = list.length;
+		for (var i = length - 1; i >= 0; i--) {
+			list[i].parentElement.click();
+		}
+		return length;
+	}
+
+	function deleteElements() {
+		document.getElementsByClassName('icon-delete')[0].click();
+		document.getElementsByClassName('btn-default')[0].click();
+	}
+
+	scrollToTop();
+	displayCheckboxes();
+	length = checkBoxes();
+	deleteElements();
+	console.log(length + ' elementos eliminados.');
+})();
