40dde7ce7673e2fdba2031c5a4be8c03b5f9f426
dgstranz <dgstranz@gmail.com>
2016-06-07 17:05:13 +0200

    reformatted

--- msgSelectAll.js
+++ msgSelectAll.js
@@ -1,40 +1,40 @@
-(function() {
-	function scrollToTop() {
-		document.getElementsByClassName('pane-chat-body')[0].scrollTop = 0;
-	}
+var scrollToTop = function() {
+	document.getElementsByClassName('pane-chat-body')[0].scrollTop = 0;
+}
 
-	function displayCheckboxes() {
-		document.getElementsByClassName('icon-menu')[1].click();
-		document.getElementsByClassName('menu-shortcut')[1].click();
-	}
+var displayCheckboxes = function() {
+	document.getElementsByClassName('icon-menu')[1].click();
+	document.getElementsByClassName('menu-shortcut')[1].click();
+}
 
-	function checkBoxes() {
-		var list = document.getElementsByClassName('unchecked');
-		var length = list.length;
-		for (var i = length - 1; i >= 0; i--) {
-			list[i].parentElement.click();
-		}
-		return length;
+var checkBoxes = function() {
+	var list = document.getElementsByClassName('unchecked');
+	var length = list.length;
+	for (var i = length - 1; i >= 0; i--) {
+		list[i].parentElement.click();
 	}
+	return length;
+}
 
-	function deleteElements() {
-		document.getElementsByClassName('icon-delete')[0].click();
-		document.getElementsByClassName('btn-default')[0].click();
+var deleteElements = function() {
+	document.getElementsByClassName('icon-delete')[0].click();
+	document.getElementsByClassName('btn-default')[0].click();
+}
+
+var hideSystemMessages = function() {
+	var systemMessages = document.getElementsByClassName('message-system');
+
+	while (systemMessages.length) {
+		var element = systemMessages[0].parentNode;
+		element.parentNode.removeChild(element);
 	}
+}
 
-	function hideSystemMessages() {
-		var systemMessages = document.getElementsByClassName('message-system');
-		var length = systemMessages.length;
-		while (length) {
-			var element = systemMessages[0].parentNode;
-			element.parentNode.removeChild(element);
-		}
-		return length;
-	}
-
+var loadMessages = setInterval(function() {
 	scrollToTop();
-	displayCheckboxes();
-	length = checkBoxes();
-	deleteElements();
-	console.log(length + ' elementos eliminados.');
-})();
+	var qty = document.getElementsByClassName('message-chat').length;
+	console.log(qty);
+	if (qty > 2000) {
+		clearInterval(loadMessages);
+	}
+}, 1000);
